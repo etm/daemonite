17def6915eaef9f4b523b937b5069169fcdf23b1
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-07-02 15:50:28 +0200

    bug

--- lib/daemonite.rb
+++ lib/daemonite.rb
@@ -134,7 +134,6 @@ module Daemonism
             if count > opts[:kill_amount]
               Process.kill "SIGKILL", pid
               File.unlink(opts[:basepath] + '/' + opts[:pidfile])
-              @at_exit.call(opts) if @at_exit
             else
               Process.kill "SIGTERM", pid
             end
